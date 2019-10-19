899eb03b77748c94875fe43996e1fee0c88b7f36
joebuntu2 <joebuntu85@gmail.com>
2019-10-19 13:36:57 -0400

    excess-chichester-spending.component now working

--- ChichesterSchoolWatch.WebUI/ClientApp/src/app/budgets/comparisons/excess-chichester-spending/excess-chichester-spending.component.ts
+++ ChichesterSchoolWatch.WebUI/ClientApp/src/app/budgets/comparisons/excess-chichester-spending/excess-chichester-spending.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-excess-chichester-spending',
   templateUrl: './excess-chichester-spending.component.html',
   styleUrls: ['./excess-chichester-spending.component.css']
 })
-export class ExcessChichesterSpendingComponent implements OnInit {
+export class ExcessChichesterSpendingComponent {
+ 
+  public isBusy: boolean;
+  public districts: any[];
+  public comparisons: any[];
+  public objectKeys = Object.keys;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient, 
+    @Inject('BASE_URL') baseUrl: string) {
+  
+    this.isBusy = true;
+ 
+    forkJoin([
+      http.get<any[]>(baseUrl + 'api/DistrictComparisons'),
+      http.get<any[]>(baseUrl + 'api/Districts') 
+    ]).subscribe(results => {
 
-  ngOnInit() {
+        this.comparisons = results[0];
+ 
+        let districtsResults = results[1];
+        this.districts = districtsResults.sort((a, b) => a.name.localeCompare(b.name));
+
+        this.isBusy = false;
+      },
+      error => console.error(error)
+    );
   }
 
 }
