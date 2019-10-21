c8cd394cb67575b0f8084502fd25d42b298f5dfa
joebuntu2 <joebuntu85@gmail.com>
2019-10-20 20:19:20 -0400

    excess chi spending now defaults to penn delco

--- ChichesterSchoolWatch.WebUI/ClientApp/src/app/budgets/comparisons/excess-chichester-spending/excess-chichester-spending.component.ts
+++ ChichesterSchoolWatch.WebUI/ClientApp/src/app/budgets/comparisons/excess-chichester-spending/excess-chichester-spending.component.ts
@@ -11,7 +11,7 @@ export class ExcessChichesterSpendingComponent {
  
   public isBusy: boolean;
   public districts: any[];
-  public comparisons: any[];
+  public comparisons: any;
   public objectKeys = Object.keys;
   public currentDistrict: any;
   public currentMetric: any;
@@ -19,23 +19,34 @@ export class ExcessChichesterSpendingComponent {
   constructor(
     private http: HttpClient,
     @Inject('BASE_URL') baseUrl: string) {
-  
+
     this.isBusy = true;
- 
-    forkJoin([
-      http.get<any[]>(baseUrl + 'api/DistrictComparisons'),
-      http.get<any[]>(baseUrl + 'api/Districts') 
-    ]).subscribe(results => {
 
-        this.comparisons = results[0];
- 
-        let districtsResults = results[1];
-        this.districts = districtsResults.sort((a, b) => a.name.localeCompare(b.name));
+    forkJoin(
+        http.get<any>(baseUrl + 'api/DistrictComparisons'),
+        http.get<any[]>(baseUrl + 'api/Districts')
+      )
+      .subscribe(([comparisons, districts]) => {
+          this.comparisons = comparisons;
+          this.districts = districts.sort((a, b) => a.name.localeCompare(b.name));
 
-        this.isBusy = false;
-      },
-      error => console.error(error)
-    );
+   
+            let match = this.comparisons.districtFiscalYearMetrics
+              .find(x =>
+                x.district.name.toLowerCase().startsWith("PennDelco".toLowerCase()
+                ));
+
+            if (match != null) {
+              let district = match.district;
+              let fyMetrics = match.metricsByFiscalYear[5]; //2019-2020 metrics
+              let excessSpendingMetrics = fyMetrics.metrics.ExcessChichesterSpending;
+              this.setCurrent(district, excessSpendingMetrics);
+            } 
+ 
+          this.isBusy = false;
+        },
+        error => console.error(error)
+      );
   }
 
   setCurrent(district: any, metric: any) {
