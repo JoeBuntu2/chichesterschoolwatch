bc2b172930a77c3d849340439cd1026e524d8902
joebuntu2 <joebuntu85@gmail.com>
2019-10-24 12:54:12 -0400

    improvements to grid

--- ChichesterSchoolWatch.WebUI/ClientApp/src/app/components/comparisons-grid/comparisons-grid.component.ts
+++ ChichesterSchoolWatch.WebUI/ClientApp/src/app/components/comparisons-grid/comparisons-grid.component.ts
@@ -9,13 +9,28 @@ export class ComparisonsGridComponent implements OnInit {
 
   @Input() metric: string;
   @Input() comparisons: any;
+  @Input() isPercent: boolean;
 
-  public objectKeys = Object.keys;
+  public fiscalYears: any[]; 
 
 
-  constructor() { }
+  constructor() {
+
+    this.fiscalYears = [];
+  }
 
   ngOnInit() {
+    var keys = Object.keys(this.comparisons.fiscalYears);
+    keys.sort((a, b) => a.localeCompare(b) * -1);
+
+    keys.forEach(key => {
+      this.fiscalYears.push(
+        {
+          key: key,
+          year: this.comparisons.fiscalYears[key]
+
+        });
+    }); 
   }
 
 }
