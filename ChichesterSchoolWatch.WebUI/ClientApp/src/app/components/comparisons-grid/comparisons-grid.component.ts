1ef7e7443bd75935688a60faf0dc29520d44b8c8
Bern <bernie@socketlabs.com>
2019-10-24 09:11:22 -0400

    Working on mobile design

---
+++ ChichesterSchoolWatch.WebUI/ClientApp/src/app/components/comparisons-grid/comparisons-grid.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-comparisons-grid',
+  templateUrl: './comparisons-grid.component.html',
+  styleUrls: ['./comparisons-grid.component.css']
+})
+export class ComparisonsGridComponent implements OnInit {
+
+  @Input() metric: string
+  @Input() comparisons: any[];
+
+  public objectKeys = Object.keys;
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
