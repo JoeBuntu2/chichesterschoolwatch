d90437be91cf1567e4b596b6e836f1fb5cfb5b3c
joebuntu2 <joebuntu85@gmail.com>
2019-10-24 11:15:29 -0400

    build fix

--- ChichesterSchoolWatch.WebUI/ClientApp/src/app/components/comparisons-grid/comparisons-grid.component.ts
+++ ChichesterSchoolWatch.WebUI/ClientApp/src/app/components/comparisons-grid/comparisons-grid.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ComparisonsGridComponent implements OnInit {
 
-  @Input() metric: string
-  @Input() comparisons: any[];
+  @Input() metric: string;
+  @Input() comparisons: any;
 
   public objectKeys = Object.keys;
 
