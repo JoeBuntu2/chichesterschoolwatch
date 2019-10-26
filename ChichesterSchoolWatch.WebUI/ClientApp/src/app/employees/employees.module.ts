a159789a089eba7a65be6ed3f7e3196c941ed64f
joebuntu2 <joebuntu85@gmail.com>
2019-10-26 13:52:27 -0400

    using busy indicator everywhere

--- ChichesterSchoolWatch.WebUI/ClientApp/src/app/employees/employees.module.ts
+++ ChichesterSchoolWatch.WebUI/ClientApp/src/app/employees/employees.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SalariesComponent } from './salaries/salaries.component';
 import { EmployeesComponent } from './employees.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 let routing = RouterModule.forChild([
   { path: "salaries", component: SalariesComponent },
   { path: "", component: EmployeesComponent }
 ])
 @NgModule({
-  imports: [CommonModule,   routing], 
+  imports: [CommonModule,   routing, SharedModule], 
   declarations: [SalariesComponent, EmployeesComponent]
 }) 
 export class EmployeesModule { }
