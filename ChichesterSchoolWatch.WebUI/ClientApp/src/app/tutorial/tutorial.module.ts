93d50a366406984982dff8466fe118ef91c2487d
joebuntu2 <joebuntu85@gmail.com>
2019-10-27 08:33:01 -0400

    adding charts.js to tutorial

--- ChichesterSchoolWatch.WebUI/ClientApp/src/app/tutorial/tutorial.module.ts
+++ ChichesterSchoolWatch.WebUI/ClientApp/src/app/tutorial/tutorial.module.ts
@@ -4,7 +4,8 @@ import { RouterModule } from '@angular/router';
 import { WhyTaxesAreHighComponent } from './why-taxes-are-high/why-taxes-are-high.component';
 import { Step1Component } from './why-taxes-are-high/step1/step1.component';
 import { Step2Component } from './why-taxes-are-high/step2/step2.component';
-import { StepHostDirective } from './why-taxes-are-high/step-host.directive'; 
+import { StepHostDirective } from './why-taxes-are-high/step-host.directive';
+import { ChartsModule } from 'ng2-charts';
 
 let routing = RouterModule.forChild([
   { path: "tax-payers-guide-to-high-chi-taxes", component: WhyTaxesAreHighComponent }
@@ -16,6 +17,7 @@ let routing = RouterModule.forChild([
   ],
   imports: [
     routing,
+    ChartsModule,
     CommonModule
   ],
   exports:[Step1Component, Step2Component],
--- ChichesterSchoolWatch.WebUI/ClientApp/src/app/tutorial/why-taxes-are-high/step1/step1.component.ts
+++ ChichesterSchoolWatch.WebUI/ClientApp/src/app/tutorial/why-taxes-are-high/step1/step1.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Injectable } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import * as pluginDataLabels from 'chartjs-plugin-datalabels';
+import { Label } from 'ng2-charts';
 
-@Injectable({
-  providedIn: 'root'
-})
+ 
 @Component({
   selector: 'app-step1',
   templateUrl: './step1.component.html',
@@ -10,9 +11,30 @@ import { Component, OnInit, Injectable } from '@angular/core';
 })
 export class Step1Component implements OnInit {
 
-  constructor() { }
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+      display: false
+    },
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: { xAxes: [{}], yAxes: [{}] },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      }
+    }
+  };
+  public barChartLabels: Label[] = ['School', 'Township', 'County'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartPlugins = [pluginDataLabels];
+
+  public barChartData: ChartDataSets[] = [
+    { data: [79, 10, 11] }
+  ];
+
 
   ngOnInit() {
-  }
-
+  }  
 }
