2094d0adac7a85f4d9fcb1bf25f90120091ab722
joebuntu2 <joebuntu85@gmail.com>
2019-10-26 22:44:25 -0400

    adding ability to download records for a particular comparison type

--- ChichesterSchoolWatch.WebUI/Controllers/DistrictComparisonsController.cs
+++ ChichesterSchoolWatch.WebUI/Controllers/DistrictComparisonsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using SchoolWatch.Business.DTO.DistrictComparisons; 
@@ -20,8 +23,7 @@ namespace ChichesterSchoolWatch.WebUI.Controllers
             DistrictComparisonService = districtComparisonService;
             Logger = logger;
         }
-
-        // - 8 hour cache
+ 
         [HttpGet] 
         [ResponseCache(CacheProfileName = "StaticData12Hr")]
         public AllDistrictComparisonsDto Get( )
@@ -29,5 +31,39 @@ namespace ChichesterSchoolWatch.WebUI.Controllers
             Logger.LogDebug("Made it past cache...fetching data");
             return DistrictComparisonService.GetAll();
         }
+ 
+        [HttpGet("download")]
+        public List<DownloadRecord> Download( ComparisonType comparisonType)
+        { 
+            
+            var all = DistrictComparisonService.GetAll();
+
+            var results = new List<DownloadRecord>();
+            foreach (var allDistrictFiscalYearMetric in all.DistrictFiscalYearMetrics)
+            {
+                foreach (var fiscalYearMetric in allDistrictFiscalYearMetric.MetricsByFiscalYear)
+                {
+                    var value = fiscalYearMetric.Value.Metrics[comparisonType];
+
+                    results.Add(new DownloadRecord
+                    {
+                        Disrict = allDistrictFiscalYearMetric.District.Name,
+                        FiscalYear = all.FiscalYears[fiscalYearMetric.Key],
+                        Value = value
+                    });
+                }
+            }
+
+            return results;
+        }
+
+        public class DownloadRecord
+        {
+            public string Disrict { get; set; }
+            public string FiscalYear { get; set; }
+            public object Value { get; set; }
+        }
+
+
     }
 }
