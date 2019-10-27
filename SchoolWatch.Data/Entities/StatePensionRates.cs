59a00897b1eea03728e38ff618c392e0f6bcbd48
joebuntu2 <joebuntu85@gmail.com>
2019-10-27 08:35:18 -0400

    adding scripts

---
+++ SchoolWatch.Data/Entities/StatePensionRates.cs
@@ -0,0 +1,9 @@
+﻿
+namespace SchoolWatch.Data.Entities
+{
+    public class StatePensionRateEntity
+    {
+        public int FiscalYearId { get; set; }
+        public decimal TotalContributionRate { get; set; }
+    }
+}
