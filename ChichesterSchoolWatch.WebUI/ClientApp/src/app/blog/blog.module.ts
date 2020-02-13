1ebdd84f0dcae5c424f77d54f3c68187e51a94d5
joebuntu2 <joebuntu85@gmail.com>
2020-02-13 18:19:25 -0500

    start of psers #3

--- ChichesterSchoolWatch.WebUI/ClientApp/src/app/blog/blog.module.ts
+++ ChichesterSchoolWatch.WebUI/ClientApp/src/app/blog/blog.module.ts
@@ -5,13 +5,15 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { PsersNarrativePart1Component } from './psers-narrative-part1/psers-narrative-part1.component';
 import { PsersNarrativePart2Component } from './psers-narrative-part2/psers-narrative-part2.component';
+import { PsersNarrativePart3Component } from './psers-narrative-part3/psers-narrative-part3.component';
 
 let routing = RouterModule.forChild([
   { path: "2020/2/6/psers-narrative-part-1-actual-impact", component: PsersNarrativePart1Component },
-  { path: "2020/2/11/psers-narrative-part-2-bartholf-misleads", component: PsersNarrativePart2Component }
+  { path: "2020/2/11/psers-narrative-part-2-bartholf-misleads", component: PsersNarrativePart2Component },
+  { path: "2020/2/11/psers-narrative-local-press-reports-misleading-figures", component: PsersNarrativePart2Component }
 ]);
 @NgModule({
-  declarations: [PsersNarrativePart1Component, PsersNarrativePart2Component],
+  declarations: [PsersNarrativePart1Component, PsersNarrativePart2Component, PsersNarrativePart3Component],
   imports: [
     CommonModule,
     routing,
---
+++ ChichesterSchoolWatch.WebUI/ClientApp/src/app/blog/psers-narrative-part3/psers-narrative-part3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-psers-narrative-part3',
+  templateUrl: './psers-narrative-part3.component.html',
+  styleUrls: ['./psers-narrative-part3.component.css']
+})
+export class PsersNarrativePart3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
